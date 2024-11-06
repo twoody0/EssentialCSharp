63b9f8c8b1305ae461b6e6d17c849ae5b9801ce6
Windeko <tylerjameswoody@gmail.com>
2024-11-06 10:32:15 -0800

    Added a test for Chapter 13, Listing 13.15

---
+++ src/Chapter13.Tests/Listing13.15.Tests.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter13.Listing13_15.Tests;
+
+[TestClass]
+public class ProgramTests
+{
+    [TestMethod]
+    public void MainTest()
+    {
+        const string expectedInput = "Hello, World!";
+        const string expectedOutput = "Hello, World!";
+
+        StringReader reader = new (expectedInput);
+        StringWriter writer = new ();
+        try
+        {
+            Console.SetIn(reader);
+            Console.SetOut(writer);
+            Program.Main();
+
+            string actualOutput = writer.ToString().Trim();
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+        finally
+        {
+            reader.Dispose();
+            writer.Dispose();
+        }
+    }
+}
